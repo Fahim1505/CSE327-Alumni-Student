cbfd112eca7d0a7fb316ecbceb146280c8c1b525
Julfikar Utshab <julfikarutshab@gmail.com>
2025-12-01 03:06:46 +0600

    Update web.php

--- Resource/web.php
+++ Resource/web.php
@@ -1,1 +1,3 @@
-
+use App\Http\Controllers\Auth\RegisterController;
+Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
