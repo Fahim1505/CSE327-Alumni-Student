4b365b96f1beeca7ffcc972625cc2113703457c1
Julfikar Utshab <julfikarutshab@gmail.com>
2025-12-01 03:10:09 +0600

    Delete Resource/web.php

--- Resource/web.php
+++
@@ -1,3 +0,0 @@
-use App\Http\Controllers\Auth\RegisterController;
-Route::get('/register', [RegisterController::class, 'show'])->name('register.show');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
