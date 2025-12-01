95310c58e97bb532ce3041e56e42be555a0d4b67
Julfikar Utshab <julfikarutshab@gmail.com>
2025-12-01 19:18:43 +0600

    Update web.php

--- Routes/web.php
+++ Routes/web.php
@@ -17,6 +17,27 @@ Route::get('/logout', function(){
 });
 
 use App\Http\Controllers\EventController;
+
+Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
+Route::post('/event/store', [EventController::class, 'store'])->name('event.store');
+Route::get('/events', [EventController::class, 'index'])->name('event.index');
+
+// New Routes for Edit & Delete
+Route::get('/event/{id}/edit', [EventController::class, 'edit'])->name('event.edit');
+Route::post('/event/{id}/update', [EventController::class, 'update'])->name('event.update');
+Route::get('/event/{id}/delete', [EventController::class, 'destroy'])->name('event.delete');
+
+use App\Http\Controllers\AnnouncementController;
+
+Route::get('/announcement/create', [AnnouncementController::class, 'create'])->name('announcement.create');
+Route::post('/announcement/store', [AnnouncementController::class, 'store'])->name('announcement.store');
+Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcement.index');
+Route::get('/announcement/{id}/edit', [AnnouncementController::class, 'edit'])->name('announcement.edit');
+Route::post('/announcement/{id}/update', [AnnouncementController::class, 'update'])->name('announcement.update');
+Route::get('/announcement/{id}/delete', [AnnouncementController::class, 'destroy'])->name('announcement.delete');
+
+
+use App\Http\Controllers\EventController;
 Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
 Route::post('/event/store', [EventController::class, 'store'])->name('event.store');
 Route::get('/events', [EventController::class, 'index'])->name('event.index');
