c1d6b4a22e0a874f938e04eed86a569062a0a13b
Julfikar Utshab <julfikarutshab@gmail.com>
2025-12-01 19:15:50 +0600

    Add files via upload

---
+++ Resource/Views/Announcement/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Announcement</title>
+</head>
+<body>
+
+<h2>Add Announcement</h2>
+
+@if(session('success'))
+    <p style="color:green">{{ session('success') }}</p>
+@endif
+
+<form action="{{ route('announcement.store') }}" method="POST">
+    @csrf
+
+    <label>Subject:</label><br>
+    <input type="text" name="subject" required><br><br>
+
+    <label>Date:</label><br>
+    <input type="date" name="date" required><br><br>
+
+    <label>Description:</label><br>
+    <textarea name="description" rows="4" required></textarea><br><br>
+
+    <button type="submit">Add Announcement</button>
+</form>
+
+<br>
+<a href="{{ route('announcement.index') }}">View All Announcements</a>
+
+</body>
+</html>
