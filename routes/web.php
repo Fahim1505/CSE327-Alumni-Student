bc34560a6fd4dcde43adec222d5daa2f77b3a3ca
Israt24hub <israt.srity@northsouth.edu>
2025-12-02 22:30:05 +0600

    Final fixes: routes, controllers, migrations, UI pending

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobListingController;
+use App\Http\Controllers\AchievementController; // Only import once
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/jobs');
 });
 
-// Correct CRUD route for your 4 BLADE FILES
+// Job CRUD routes
 Route::resource('jobs', JobListingController::class);
-
-use App\Http\Controllers\AchievementController;
-
-Route::resource('achievements', AchievementController::class);
-
-use App\Http\Controllers\AchievementController;
 
 // Achievement CRUD routes
 Route::resource('achievements', AchievementController::class);
