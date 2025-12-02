ea6e5c64c89a16f64e28e06319e12c2c362eb6c3
ahnafim <ahnafim2001@gmail.com>
2025-12-02 22:44:17 +0600

    Update donation for Students.

--- Resource/Views/Students/donate.blade.php
+++ Resource/Views/Students/donate.blade.php
@@ -1,68 +1,64 @@
-@extends('layouts.app')
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Donation</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
 
-@section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div id="side-nav" style="height: auto !important;" class="col-lg-2">
-                <div id="side-nav-overlay">
-                    @include('inc.student-sidenav')
-                </div>
-            </div>
-            <div class="col-lg-10">
-                <a href="/student-dashboard">Go back &nbsp; <i class="fa fa-chevron-circle-left"></i></a>
+<div class="container">
 
-                {{--<p>You have contributed <b>550</b> Ksh</p>--}}
-                <div class="row">
-                    <div class="col-lg-4">
-                        <br><br>
-                        <h5>Generate report of contributions</h5>
-                        <hr>
-                        <p>Click here to generate report of the amount contributed and how they were used.</p>
+    <h1>Create Donation</h1>
 
-                        <br>
-                        <a style="height: 40px;width: 50%;font-size: 15px;" class="btn btn-warning" href="/student-Donate/downloadPDF">Generate</a>
-                        {{--<center>--}}
-                            {{--<img style="max-height: 100px" src="{{asset('images/lipa.png')}}" alt="">--}}
-                        {{--</center>--}}
-                        {{--<ul>--}}
-                            {{----}}
-                                {{--Head to mpesa--}}
-                            {{----}}
-                            {{--<li>1. Choose Pay Bill</li>--}}
-                            {{--<li>2. Enter business number 	601394</li>--}}
-                            {{--<li>3. Enter account number testpai601394</li>--}}
-                            {{--<li>4. Enter amount you want to donate</li>--}}
-                            {{--<li>5. Wait for response from MPESA.</li>--}}
-                        {{--</ul>--}}
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
 
-                    </div>
-                    <div class="col-lg-8">
-                        <center>
-                            <img style="max-height: 100px" src="{{asset('paypal.jpg')}}" alt="">
-                        </center>
-                        <form method="post" action="/student-Donate/donate-paypal">
-                            {{csrf_field()}}
-                            <div class="form-group">
-                                <label for="amount">Amount</label>
-                                <input required name="amount" class="form-control" type="number">
-                            </div>
-                            
-                            <div class="form-group">
-                                Purpose <br>
-                                <select required name="purpose" class="custom-select" id="inputGroupSelect01">
-                                    <option selected>Choose purpose</option>
-                                    <option value="fees">Fees</option>
+    <form action="{{ route('donation.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
 
-                                </select>
-                            </div>
-                            <br><br>
-                            <button type="submit" style="height: 60px;width:100%;font-size: 15px;" class="btn btn-primary btn-block" href="">Donate with paypal</button>
-                        </form>
-
-
-                    </div>
-                </div>
-            </div>
+        <div>
+            <label>Donation ID</label>
+            <input type="number" name="donation_id" value="{{ old('donation_id') }}">
         </div>
-    </div>
-    @endsection
+
+        <div>
+            <label>Donation Type</label>
+            <select name="donation_type" required>
+                <option value="">Select type</option>
+                @foreach(['Money','food','cloth','Books','Equipment','Other'] as $type)
+                    <option value="{{ $type }}" {{ old('donation_type') == $type ? 'selected' : '' }}>
+                        {{ $type }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
+
+        <div>
+            <label>Description</label>
+            <textarea name="description" rows="4">{{ old('description') }}</textarea>
+        </div>
+
+        <div>
+            <label>Image</label>
+            <input type="file" name="image" accept="image/*">
+        </div>
+
+        <button type="submit">Save Donation</button>
+    </form>
+
+    <br>
+
+    <a href="{{ route('donation.index') }}">View all donations</a>
+
+</div>
+
+<script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
