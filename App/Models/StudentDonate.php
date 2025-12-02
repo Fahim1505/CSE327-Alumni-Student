dee33ec4354fe38532530ca852e58378a50fc419
ahnafim <ahnafim2001@gmail.com>
2025-12-02 22:41:20 +0600

    Update StudentDonate.php

--- App/Models/StudentDonate.php
+++ App/Models/StudentDonate.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
-class Donate extends Model
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+class StudentDonate extends Model
 {
-    //
+    //class Donation extends Model { 
+    use HasFactory; 
+    protected $table = 'donation'; 
+    public $timestamps = true; 
+    protected $fillable = [ 'donation_id', 'donation_type', 'description', 'image', ];}
 }
