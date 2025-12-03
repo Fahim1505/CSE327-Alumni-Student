2b14fd999be546fdfed170d51d05b476d001fd3a
ahnafim <ahnafim2001@gmail.com>
2025-12-03 19:28:14 +0600

    Update 2018_08_23_142719_create_students_table.php

--- Database/Migrations/Ahnaf/2018_08_23_142719_create_students_table.php
+++ Database/Migrations/Ahnaf/2018_08_23_142719_create_students_table.php
@@ -15,20 +15,10 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('First_name');
-            $table->string('Middle_name');
-            $table->string('Surname');
-            $table->string('Course');
-            $table->string('RegNo')->unique();
-            $table->string('Phone');
-            $table->string('DOB');
-            $table->string('Year_joined');
-            $table->string('County');
-            $table->string('Avatar');
-            $table->string('password');
-            $table->string('email');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('admission_year');
+            $table->string('current_semester');
+            $table->string('division');
         });
     }
 
