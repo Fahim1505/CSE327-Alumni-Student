445b67a1ee1d5bda31ed4229790d77f0b2a8e442
ananna77 <ananna.saha@northsouth.edu>
2025-12-03 20:45:47 +0600

    Update PhotoGallery.php

--- app/Models/PhotoGallery.php
+++ app/Models/PhotoGallery.php
@@ -1,1 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PhotoGallery extends Model
+{
+    protected $fillable = [
+        'name',
+        'filePath',
+        'caption',
+        'graduationYear',
+        'uploadedAt',
+    ];
+}
 
