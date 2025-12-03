0371542cb855e3a419b9dbe4d9d3ecaf87f96597
ahnafim <ahnafim2001@gmail.com>
2025-12-03 18:52:55 +0600

    Update StudentTest with the correct unit testing syntax

--- tests/Unit/StudentTest.php
+++ tests/Unit/StudentTest.php
@@ -10,13 +10,16 @@ class StudentTest extends TestCase
     /**
      * Test whether the function returns the full name of the student
      */
-    public function test_full_name_of_student(): void
+    public function test_full_details_of_registered_student(): void
     {
         $student = new Student([
-            'First_name'=> 'Ahnaf',
-            'Middle_name' => 'Nafim',
-            'Surname' => 'Shiraj'
+            'name'=> 'Ahnaf Nafim Shiraj',
+            'admission_year' => '2021',
+            'current_semester' => '11th',
+            'division' => 'Dhaka',
+            'student_id' => '2131800642'
+
         ]);
-        $this->assertEquals('Ahnaf Nafim Shiraj', $student->fullName());
+        $this->assertEquals('Ahnaf Nafim Shiraj, 2021, 11th, Dhaka, 2131800642', $student->studentDetails());
     }
 }
