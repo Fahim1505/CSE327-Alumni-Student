411762f1cc6774b442619042ef962ce9aa4e3d4c
ahnafim <ahnafim2001@gmail.com>
2025-12-03 18:42:51 +0600

    Unit testing of Student

--- tests/Unit/StudentTest.php
+++ tests/Unit/StudentTest.php
@@ -2,15 +2,21 @@
 
 namespace Tests\Unit;
 
+use App\Student;
 use PHPUnit\Framework\TestCase;
 
 class StudentTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * Test whether the function returns the full name of the student
      */
-    public function test_example(): void
+    public function test_full_name_of_student(): void
     {
-        $this->assertTrue(true);
+        $student = new Student([
+            'First_name'=> 'Ahnaf',
+            'Middle_name' => 'Nafim',
+            'Surname' => 'Shiraj'
+        ]);
+        $this->assertEquals('Ahnaf Nafim Shiraj', $student->fullName());
     }
 }
