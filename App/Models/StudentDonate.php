520bad6431f49d16c385dddd179447bc75db9968
ahnafim <ahnafim2001@gmail.com>
2025-12-03 19:59:15 +0600

    Update StudentDonate.php

--- App/Models/StudentDonate.php
+++ App/Models/StudentDonate.php
@@ -1,14 +1,26 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
-class StudentDonate extends Model
+
+class StudentDonation extends Model
 {
-    //class Donation extends Model { 
-    use HasFactory; 
-    protected $table = 'donation'; 
-    public $timestamps = true; 
-    protected $fillable = [ 'donation_id', 'donation_type', 'description', 'image', ];}
+    use HasFactory;
+
+    protected $table = 'donation';
+
+    protected $fillable = [
+        'student_id',
+        'donation_type',
+        'description',
+        'image',
+        'amount',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
